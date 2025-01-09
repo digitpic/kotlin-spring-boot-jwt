1a034b35f7c2d272051b61bf774142d1698ac1af
digitpic <jkw5033@gmail.com>
2025-01-09 20:45:27 +0900

    refactor(redis-config): 코드 최적화

--- src/main/kotlin/com/group/jwt/redis/RedisConfig.kt
+++ src/main/kotlin/com/group/jwt/redis/RedisConfig.kt
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.CacheKeyPrefix
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
@@ -22,42 +24,49 @@ class RedisConfig(
 
     @Value("\${spring.data.redis.port}")
     val port: Int,
+
+    @Value("\${spring.data.redis.cache.ttl}")
+    val cacheTtl: Long
 ) {
     @Bean
-    fun lettuceConnectionFactory(): LettuceConnectionFactory {
-        val redisStandaloneConfiguration = RedisStandaloneConfiguration(host, port)
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val standaloneConfiguration = RedisStandaloneConfiguration(host, port)
 
-        // Jedis, Lettuce 중 성능이 더 좋은 Lettuce 사용
-        val lettuceClientConfiguration = LettuceClientConfiguration.builder()
+        val clientConfiguration = LettuceClientConfiguration.builder()
             .commandTimeout(Duration.ZERO)
             .shutdownTimeout(Duration.ZERO)
             .build()
 
-        return LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration)
+        return LettuceConnectionFactory(standaloneConfiguration, clientConfiguration)
     }
 
     @Bean
-    fun redisTemplate(): RedisTemplate<*, *> {
-        return RedisTemplate<Any, Any>().apply {
-            this.connectionFactory = lettuceConnectionFactory()
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>()
+        redisTemplate.connectionFactory = redisConnectionFactory
 
-            // prefix 해시 값 나오지 않도록 serializer 설정 (ex. "\xac\xed\x00")
-            this.keySerializer = StringRedisSerializer()
-            this.hashKeySerializer = StringRedisSerializer()
-            this.valueSerializer = StringRedisSerializer()
-        }
+        // 일반 키와 값 직렬화
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer()
+
+        // 해시된 키와 값 직렬화
+        redisTemplate.hashKeySerializer = StringRedisSerializer()
+        redisTemplate.hashValueSerializer = GenericJackson2JsonRedisSerializer()
+
+        return redisTemplate
     }
 
     @Bean
-    fun cacheManager(): CacheManager {
+    fun cacheManager(redisConnectionFactory: RedisConnectionFactory): CacheManager {
         val configuration = RedisCacheConfiguration.defaultCacheConfig()
-            .serializeKeysWith(fromSerializer(StringRedisSerializer()))  // key serialize
-            .serializeValuesWith(fromSerializer(GenericJackson2JsonRedisSerializer())) // value serialize
-            .entryTtl(Duration.ofMinutes(2)) // 캐시 기본 TTL 2분 설정
+            .serializeKeysWith(fromSerializer(StringRedisSerializer()))  // 키 직렬화
+            .serializeValuesWith(fromSerializer(GenericJackson2JsonRedisSerializer())) // 값 직렬화
+            .entryTtl(Duration.ofMinutes(cacheTtl))
             .disableCachingNullValues() // null data 캐싱 제외
+            .computePrefixWith(CacheKeyPrefix.simple()) // 캐시 키 prefix 지정
 
         return RedisCacheManager.RedisCacheManagerBuilder
-            .fromConnectionFactory(lettuceConnectionFactory())
+            .fromConnectionFactory(redisConnectionFactory)
             .cacheDefaults(configuration)
             .build()
     }
