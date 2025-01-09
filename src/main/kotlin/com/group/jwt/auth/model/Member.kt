2d9b2aba1346b3f41090644502345486ba627571
digitpic <jkw5033@gmail.com>
2025-01-09 21:52:04 +0900

    refactor(member): import 문 asterisk 삭제

--- src/main/kotlin/com/group/jwt/auth/model/Member.kt
+++ src/main/kotlin/com/group/jwt/auth/model/Member.kt
@@ -1,6 +1,11 @@
 package com.group.jwt.auth.model
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
